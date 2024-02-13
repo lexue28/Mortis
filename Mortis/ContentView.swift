bf898c5bb292a6dba33c3818a529acd5c68eb398
Linda Xue <lexue28@gmail.com>
2024-02-13 15:20:13 -0500

    Initial Commit

---
+++ Mortis/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Mortis
+//
+//  Created by Linda Xue on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mortis/MortisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortisApp.swift
+//  Mortis
+//
+//  Created by Linda Xue on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MortisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
