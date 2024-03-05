3ac2f4acc1f5b269d9e10504463c140838e92d05
Linda Xue <lexue28@gmail.com>
2024-03-05 15:24:59 -0500

    hi

---
+++ Mortis/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Mortis
+//
+//  Created by Linda Xue on 3/5/24.
+//
+
+import Foundation
---
+++ Mortis/ageClass.swift
@@ -0,0 +1,23 @@
+//
+//  ageClass.swift
+//  Mortis
+//
+//  Created by Linda Xue on 3/5/24.
+//
+
+import Foundation
+import UIKit
+import TabularData
+
+final class ageClass {
+  func getStats(name : String) -> DataFrame.Slice {
+    // Columns: Id,Name,Short description,Gender,Country,Occupation,Birth year,Death year,Manner of death,Age of death
+
+  }
+  
+  func getAge(name : String) -> Int {
+    // Id,Name,Short description,Gender,Country,Occupation,Birth year,Death year,Manner of death,Age of death
+
+    return -1 // oh we couldn't find anything
+  }
+}
